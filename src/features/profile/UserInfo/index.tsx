e0bc490f0e55283237682faf9726c6ae5a26693b
Lexa <ily4.farmer@yandex.ru>
2023-12-11 16:28:30 +0300

    feat: add profile

---
+++ src/features/profile/UserInfo/index.tsx
@@ -0,0 +1,9 @@
+import { Card, CardHeader, Heading } from '@chakra-ui/react';
+
+export const UserInfo = () => (
+  <Card>
+    <CardHeader>
+      <Heading size="md">Client Report</Heading>
+    </CardHeader>
+  </Card>
+);
